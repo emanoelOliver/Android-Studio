dcde71c9ee5763fd9ba30b3c46150f546dbbf2cb
Emanoel Oliveira <emanoel.oj88@gmail.com>
2022-01-12 02:52:25 -0300

    Aprendendo o básico do Java

---
+++ 05 - Java do zero/Tipos de variaveis/source/domain/HelloWorld.java
@@ -0,0 +1,36 @@
+package domain;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        //String - textos ex "café", "escola", "Emanoel Oliveira"
+        //String minhaVariavelString = "Café";
+        
+        //Variáveis números
+        //byte - números ex: 1,2,3,20 de -128 atéa 127
+        //byte minhaVariavelbyte = 23;
+        
+        //short - números ex: 1, 180, 900 de -32768 até 32767
+        //short minhaVariavelShort = 3200;
+        
+        //int - números ex: 1, 180 de -2147483648 até 2147483647
+        //int minhaVariavelInt = 380000;
+        
+        //long - números ex: 1, 500000 
+        // de -9223372036854775808 até 9223372036854775808
+        //long minhaVariavelLong = 10000000000L;
+        
+        //float - número decimais ex: 1.2531634 / precisão de 7 casas
+        //float preco = 2.12356f;
+        
+        //double - número decimais ex: 1.3521 / precisão de 15 casas
+        //double preco = 2.158769834568532;
+        
+        //boolean - valores booleanos - true / false
+        boolean minhaVariavelBoolean = true;
+        
+        System.out.println(minhaVariavelBoolean);
+        
+    }
+
+}
---
+++ 05 - Java do zero/Variaveis e constantes/source/domain/HelloWorld.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    
+        String nome = "Emanoel Oliveira";
+        System.out.println( nome );
+        
+        int numero = 10;
+        int numero2 = 15;
+        System.out.println ( numero + numero2 );
+        
+        final int ALTURA = 187;
+        System.out.println ( ALTURA );
+        
+    }
+
+}
