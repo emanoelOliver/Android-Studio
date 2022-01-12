05c4506968c7c0d448853e0fdb5e9ec750825d8e
Emanoel Oliveira <emanoel.oj88@gmail.com>
2022-01-12 18:02:24 -0300

    Operadores Relacionais / Lógicos

---
+++ 05 - Java do zero/Operadores relacionais & logicos/source/domain/HelloWorld.java
@@ -0,0 +1,53 @@
+package domain;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        boolean resB; // True ou False.
+        int n1 = 1;
+        int n2 = 2;
+        
+        int idade = 18;
+        boolean resI; // True ou False.
+        
+        // (==) igual
+        // resB = n1 == n2;
+        // System.out.println(resB); // FALSE.
+        
+        
+        // (!=) diferente
+        // resB = n1 != n2;
+        // System.out.println(resB); // VERADEIRO.
+        
+        
+        // (<) maior
+        // resB = idade > 18;
+        // System.out.println(resI); // VERADEIRO.
+        
+        
+        // (>) menor
+        // resB = idade < 18;
+        // System.out.println(resI); // FALSE.
+        
+        
+        // (>=) maior ou igual
+        // resB = idade >= 18;
+        // System.out.println(resI); // VERDADEIRO.
+        
+        
+        // (=>) menor ou igual
+        // resB = idade => 18;
+        // System.out.println(resI); // FALSE.
+        
+        //OPERADORES LÓGICOS.
+        // (&&) e 
+        // resB = 1==1 && 2==2;
+        // System.out.prinln (resB) // VERDADEIRO.
+        
+        
+        // (||) ou
+        // resB = 1==1 && 2==2;
+        // System.out.prinln (resB) // FALSE.
+    }
+
+}
