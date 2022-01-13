4c280686be3e2a218a2a9936d467cd4db56005a9
Emanoel Oliveira <emanoel.oj88@gmail.com>
2022-01-13 05:23:32 -0300

    Programa basico de Loops - While / For / Do while

---
+++ 05 - Java do zero/Loops - Do while & For/source/domain/HelloWorld.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        /*int numero = 1; 
+        while (numero <= 5){
+            System.out.println(numero);
+            numero = numero++;
+        }*/
+        
+        for(int n = 1; n<5; n++){
+            System.out.println("N = " + n);
+        }
+       
+       /* do{
+            System.out.println(numero);
+            numero = numero + 1;
+        }while (numero <= 5);*/
+    }
+
+}
