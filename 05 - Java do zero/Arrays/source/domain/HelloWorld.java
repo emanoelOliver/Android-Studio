ac37bfacff2da769d488d8492e5a273febf7cbcd
Emanoel Oliveira <emanoel.oj88@gmail.com>
2022-01-13 03:39:27 -0300

    Simples programa de Arrays

---
+++ 05 - Java do zero/Arrays/source/domain/HelloWorld.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+       // String[] nomes = {"Emanoel", "Leticia"};
+       // int[] numeros = {20,50,80,90};
+       // boolean[] itens = {true, false, false, true};
+        
+       // System.out.println( nomes[0] );
+       // System.out.println( numeros[1] );
+       // System.out.println( itens[3] );
+    }
+
+}
